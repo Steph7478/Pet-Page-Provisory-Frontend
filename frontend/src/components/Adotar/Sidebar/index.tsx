befea0d583ebd8b2b6048f6bb1fd41c31c6b215b
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-05-29 12:04:16 -0300

    refactor(frontend): adotar page

--- frontend/src/components/Adotar/Sidebar/index.tsx
+++ frontend/src/components/Adotar/Sidebar/index.tsx
@@ -1,7 +1,15 @@
 import Checkbox from "@/ui/checkbox";
 import React from "react";
+import {BsFillFilterCircleFill} from "react-icons/bs";
+import {IoIosCloseCircleOutline} from "react-icons/io";
 
-const Sidebar = () => {
+const Sidebar = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+}) => {
   const filter = (category: string, value: string, name: string) => {
     return (
       <Checkbox
@@ -14,26 +22,71 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="bg-[var(--brown)] flex min-w-[200px] flex-col py-4 px-6 gap-6 justify-start max-[800px]:absolute max-[800px]:hidden">
-      <h2 className="text-4xl my-4 text-[var(--light-yellow)]">Filtros</h2>
-      <div className="flex flex-col gap-2 w-full">
-        <p className="font-semibold text-left">Tamanho</p>
-        <div className="flex flex-col gap-1 items-start">
-          {filter("size", "small", "pequeno")}
-          {filter("size", "medium", "medio")}
-          {filter("size", "large", "grande")}
+    <>
+      <div className="hidden md:flex bg-[var(--brown)] min-w-[200px] flex-col p-4 gap-6 justify-start ">
+        <h2 className="text-4xl my-4 text-[var(--light-yellow)]">Filtros</h2>
+
+        <div className="flex flex-col gap-2 w-full">
+          <p className="font-semibold text-left">Tamanho</p>
+          <div className="flex flex-col gap-1 items-start">
+            {filter("size", "small", "pequeno")}
+            {filter("size", "medium", "medio")}
+            {filter("size", "large", "grande")}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 w-full">
+          <p className="font-semibold text-left">Idade</p>
+          <div className="flex flex-col gap-1 items-start">
+            {filter("age", "under1", "menos de 1 ano")}
+            {filter("age", "1to5", "entre 1 a 5 anos")}
+            {filter("age", "over5", "mais de 5 anos")}
+          </div>
         </div>
       </div>
 
-      <div className="flex flex-col gap-2 w-full">
-        <p className="font-semibold text-left">Idade</p>
-        <div className="flex flex-col gap-1 items-start">
-          {filter("age", "under1", "menos de 1 ano")}
-          {filter("age", "1to5", "entre 1 a 5 anos")}
-          {filter("age", "over5", "mais de 5 anos")}
-        </div>
+      <div className="block md:hidden p-4 absolute left-0">
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <BsFillFilterCircleFill size={35} className="text-[var(--brown)]" />
+        </button>
       </div>
-    </div>
+
+      {isOpen && (
+        <div
+          className="bg-[var(--brown)] w-full max-w-[200px] p-4 max-h-[500px] absolute left-0 z-20 h-full max-[448px]:min-w-full "
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="w-full h-full relative flex justify-start max-[448px]:items-center gap-6 flex-col">
+            <IoIosCloseCircleOutline
+              onClick={() => setIsOpen(false)}
+              className="absolute right-0 text-[var(--light-yellow)] top-0"
+              size={28}
+            />
+            <h2 className="text-4xl my-4 text-[var(--light-yellow)]">
+              Filtros
+            </h2>
+
+            <div className="flex flex-col gap-2 w-full">
+              <p className="font-semibold text-left">Tamanho</p>
+              <div className="flex flex-col gap-1 items-start">
+                {filter("size", "small", "pequeno")}
+                {filter("size", "medium", "medio")}
+                {filter("size", "large", "grande")}
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2 w-full">
+              <p className="font-semibold text-left">Idade</p>
+              <div className="flex flex-col gap-1 items-start">
+                {filter("age", "under1", "menos de 1 ano")}
+                {filter("age", "1to5", "entre 1 a 5 anos")}
+                {filter("age", "over5", "mais de 5 anos")}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
