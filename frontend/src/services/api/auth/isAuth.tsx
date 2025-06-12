a1773a6ab36517000bb4f622fe822b5090c25b94
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-12 11:21:18 -0300

    refactor: new user route

--- frontend/src/services/api/auth/isAuth.tsx
+++ frontend/src/services/api/auth/isAuth.tsx
@@ -2,7 +2,7 @@ import {api} from "../client/axios";
 
 export const isAuth = async () => {
   try {
-    const res = await api.get("/auth/me");
+    const res = await api.get("/user");
     return res.data;
   } catch {
     return null;
