d2ef854346021e073c5926d7f73b2e936dcb5086
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-15 09:14:49 -0300

    fix: google

--- frontend/src/hooks/api/useOAuth.tsx
+++ frontend/src/hooks/api/useOAuth.tsx
@@ -6,16 +6,7 @@ export const useOAuth = () => {
     mutationFn: oAuth,
     onSuccess: (data) => {
       if (data?.url) {
-        const width = 600;
-        const height = 600;
-        const left = window.screen.width / 2 - width / 2;
-        const top = window.screen.height / 2 - height / 2;
-
-        window.open(
-          data.url,
-          "Login com Google",
-          `width=${width},height=${height},top=${top},left=${left}`
-        );
+        window.location.href = data.url;
       }
     },
   });
