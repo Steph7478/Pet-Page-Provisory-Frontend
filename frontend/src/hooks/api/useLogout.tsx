52eff45a39ee9638d11e8f1ee6d76ab448b1cbe3
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-14 21:24:53 -0300

    feat: query invalidation

--- frontend/src/hooks/api/useLogout.tsx
+++ frontend/src/hooks/api/useLogout.tsx
@@ -1,8 +1,13 @@
 import {logout} from "@/services/api/auth/logout";
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 
 export const useLogout = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: logout,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["auth"]});
+    },
   });
 };
