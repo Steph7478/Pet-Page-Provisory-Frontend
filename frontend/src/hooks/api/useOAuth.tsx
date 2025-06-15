ea0e4a6605ed79306fa6ed3430773863e55b86c2
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-15 09:30:21 -0300

    fix: google

--- frontend/src/hooks/api/useOAuth.tsx
+++ frontend/src/hooks/api/useOAuth.tsx
@@ -1,13 +1,14 @@
-import {oAuth} from "@/services/api/auth/oAuth";
-import {useMutation} from "@tanstack/react-query";
-
 export const useOAuth = () => {
-  return useMutation({
-    mutationFn: oAuth,
-    onSuccess: (data) => {
-      if (data?.url) {
-        window.location.href = data.url;
-      }
-    },
-  });
+  return () => {
+    const width = 600;
+    const height = 600;
+    const left = window.screen.width / 2 - width / 2;
+    const top = window.screen.height / 2 - height / 2;
+
+    window.open(
+      `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/google`,
+      "Login com Google",
+      `width=${width},height=${height},top=${top},left=${left}`
+    );
+  };
 };
