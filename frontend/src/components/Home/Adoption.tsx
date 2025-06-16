ed84360f19b565f710a4b903269c8c8b16627728
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-16 18:39:20 -0300

    delete: useless import

--- frontend/src/components/Home/Adoption.tsx
+++ frontend/src/components/Home/Adoption.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useEffect, useMemo} from "react";
+import React, {useMemo} from "react";
 import Button from "@/ui/button";
 import Image from "next/image";
 import {motion} from "framer-motion";
