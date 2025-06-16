c955bfc00a7de160459ce5c6a5d8ae22fbba081a
Maeli <maeli.palharini@hotmail.com>
2025-06-15 22:17:51 -0300

    fix: correção de CORS no backend

--- backend/src/main/java/com/patamansa/backend/config/CorsConfig.java
+++ backend/src/main/java/com/patamansa/backend/config/CorsConfig.java
@@ -14,7 +14,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://patamansasitedeadocao-production.up.railway.app",
+                                "https://pata-mansa-site-de-adocao.vercel.app",
+                                "https://pata-mansa-site-de-adocao-hwk4sz8so-maelipalharinis-projects.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
