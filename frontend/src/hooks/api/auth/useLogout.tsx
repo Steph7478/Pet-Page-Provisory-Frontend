d59db9513b857b2f0fba42301112780f82d8bfd4
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-19 17:33:00 -0300

    refactor: uselogout

--- frontend/src/hooks/api/auth/useLogout.tsx
+++ frontend/src/hooks/api/auth/useLogout.tsx
@@ -1,13 +1,15 @@
 import {logout} from "@/services/api/auth/logout";
 import {useMutation, useQueryClient} from "@tanstack/react-query";
+import router from "next/router";
 
 export const useLogout = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: logout,
-    onSuccess: () => {
-      queryClient.invalidateQueries({queryKey: ["auth"]});
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({queryKey: ["auth"]});
+      router.push("/login");
     },
   });
 };
--- frontend/src/services/api/auth/logout.tsx
+++ frontend/src/services/api/auth/logout.tsx
@@ -1,10 +1,6 @@
 import {api} from "../client/axios";
 
 export const logout = async () => {
-  try {
-    const res = await api.get("/auth/logout");
-    return res.data;
-  } catch {
-    return null;
-  }
+  const res = await api.get("/auth/logout");
+  return res.data;
 };
