30852de2385b9f603ded1633c451acd1b9418205
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-22 11:16:57 -0300

    feat: zod toast e logo text

--- frontend/src/schemas/auth.tsx
+++ frontend/src/schemas/auth.tsx
@@ -5,12 +5,12 @@ export type RegisterSchema = z.infer<typeof registerSchema>;
 
 export const loginSchema = z.object({
   userEmail: z.string().email(),
-  password: z.string().min(1),
+  password: z.string().min(6),
 });
 
 export const registerSchema = z.object({
   name: z.string().min(1),
   userEmail: z.string().email(),
-  password: z.string().min(1),
+  password: z.string().min(6, "A senha precisa no minimo 6 digitos"),
   role: z.enum(["adotante", "anunciante"]),
 });
