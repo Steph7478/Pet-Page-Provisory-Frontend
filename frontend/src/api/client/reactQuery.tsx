a66fd3b0c6718df4e41ba5f6131a99807d802dfb
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-30 08:33:02 -0300

    refactor: refresh token

--- frontend/src/api/client/reactQuery.tsx
+++ frontend/src/api/client/reactQuery.tsx
@@ -1,36 +1,40 @@
-import {mutationErrorHandler, queryErrorHandler} from "@/utils/errorHandler";
+"use client";
+
+import {ReactNode, useState} from "react";
 import {
   MutationCache,
   QueryCache,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import {ReactNode} from "react";
+import {mutationErrorHandler, queryErrorHandler} from "@/utils/errorHandler";
 
 interface ReactQueryProviderProps {
   children: ReactNode;
 }
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      refetchOnWindowFocus: false,
-    },
-    mutations: {
-      retry: false,
-    },
-  },
-
-  mutationCache: new MutationCache({
-    onError: mutationErrorHandler,
-  }),
-  queryCache: new QueryCache({
-    onError: queryErrorHandler,
-  }),
-});
-
 export function ReactQueryProvider({children}: ReactQueryProviderProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+          },
+          mutations: {
+            retry: false,
+          },
+        },
+        mutationCache: new MutationCache({
+          onError: mutationErrorHandler,
+        }),
+        queryCache: new QueryCache({
+          onError: queryErrorHandler,
+        }),
+      })
+  );
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
